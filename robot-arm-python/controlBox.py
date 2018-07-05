67686d570d34f1684194278b6c85cb9238f5a8f6
ekayiigirwa <ekayigirwa@agnesscott.edu>
2018-07-05 16:04:27 -0400

    Updated ControlBox
    I've updated the controlBox.py file to make the speed option for the "setServos " method and added the other two commands to:
    digital output by pin
    digital Input by pins
    In addition, I added the method "setArm" which linearly interpolate the alphalims (-90, 90) and musecLims (512, 2437) with few points.
    
    I have made the contolBox a stand alone class and added a new file that uses the class. The file is this usingControlBox.py

--- robot-arm-python/controlBox.py
+++ robot-arm-python/controlBox.py
@@ -1,9 +1,16 @@
 
 import serial
+import numpy as np
+import scipy as sci
+from scipy.interpolate import interp1d
 
 ## Documentation for class ControlBox
 
 class ControlBox:
+    # class properties
+    alphaLims = np.array([-90,0,90])
+    musecLims = np.array([512,1600,2437])
+    
     ## The constructor.
     def __init__ (self, portName, baud):
         self.ser = serial.Serial()
@@ -16,23 +23,46 @@ class ControlBox:
     def connect(self):
         self.ser.open()
 
-    ## setServor method to move the servor    
+    ## setServos method to move the servor    
     ## @param mid is the id number connected to the motor
-    ## @param mcmd is the rotation direction of the motor
-        
-    def setServor(self,mid, mcmd):
-        if len(mid) == len(mcmd):
-            for i,m in zip(mid, mcmd):
-                self.ser.write('"{}"'.format('E;m.'+str(i)+'.'+str(m) + ';'))
-        # what to do if there is no motor connected to the id?
-        #what happens if there is more ids than connected motors?
-        
+    ## @param mcmd is the rotation direction of the motor     
+       
+
+    def setServos(self,mid, mcmd, speed = 255):
+        if speed >0 and speed < 256:
+            if len(mid) == len(mcmd):
+                for i,m in zip(mid, mcmd):
+                    #print ('"{}"'.format('E;m.'+str(i)+'.'+str(m) + ';'))
+                    self.ser.write('"{}"'.format('E;m.'+str(i)+'.'+str(m) + ';'))
+            # what to do if there is no motor connected to the id?
+            #what happens if there is more ids than connected motors?
+        else:
+            print "speed must be between 0 and 255."
+
+    def digitalIn(self, function, mid):
+        if function == 'a' or function == 'i':
+            for i in mid:
+                self.ser.write('"{}"'.format('E;a.'+str(function)+'.'+str(i) + ';'))
+        else:
+            print "The function " + '"{}"'.format(function).upper() +  " does not exist."
+
+    def digitalOut(self, d_outPut, is_on):
+        if is_on == 1 or is_on == 0:
+            self.ser.write('"{}"'.format('E;o.' + str(d_outPut) + str(is_on) + ';'))
+        else:
+            print ('Inappropriate Arguments')          
+                
+            
     # the method to close the port    
     def shutDown(self):
         self.ser.close()
 
-myBox = ControlBox('COM3', 9600)
-myBox.connect()
-myBox.setServor([1,2,6],[1600,2000,1000])
-myBox.shutDown()
+    # a method to set the arm
+    
+    def setArm(self):
+        ticks = interp1d(self.alphaLims, self.musecLims, axis = 0,
+                         fill_value = "extrapolate")
+        print "These are the angles and corresponding positions"
+
+
 
