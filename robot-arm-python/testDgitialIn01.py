6c2a1be1c786c613233ba4b63bb3d3e754f8f934
ekayiigirwa <ekayigirwa@agnesscott.edu>
2018-07-18 15:09:55 -0400

    Add files via upload

--- robot-arm-python/testDgitialIn01.py
+++ robot-arm-python/testDgitialIn01.py
@@ -5,25 +5,18 @@ myBox = ControlBox('COM3', 9600)
 myBox.connect()
 
 timer = 0
-#while timer != 1:
-    #myBox.analogIn('a',[4,5,6])
+
 dvals = myBox.digitalIn('i', [1,2,3])
 print dvals
 print len(dvals)
-while timer != 5:
-    for el in dvals:
-        print len(el)
-        #print el.index(2)
-        #print el.index(str(0))
-        print el[10], el[15]
-        myBox.digitalOut(el[10], el[15])
-
+while timer != 30:
+    myBox.digitalOutList(dvals[0], dvals[1])
     sleep(1)
     timer += 1
 
 
 print ('Done')
-
+myBox.digitalOutList(dvals[0], [0]*len(dvals[0]))
 myBox.shutDown()
 
 
