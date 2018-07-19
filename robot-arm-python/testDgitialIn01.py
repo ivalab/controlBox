852099074f42afb341f762a68fdc0c8f5ff1a04b
ekayiigirwa <ekayigirwa@agnesscott.edu>
2018-07-19 16:20:09 -0400

    Update testDgitialIn01.py

--- robot-arm-python/testDgitialIn01.py
+++ robot-arm-python/testDgitialIn01.py
@@ -6,17 +6,17 @@ myBox.connect()
 
 timer = 0
 
-dvals = myBox.digitalIn('i', [1,2,3])
-print dvals
-print len(dvals)
-while timer != 30:
-    myBox.digitalOutList(dvals[0], dvals[1])
+
+
+while timer != 100:
+    dvals = myBox.digitalIn('i', [1,2,3])
+    print(dvals)
+    myBox.digitalOutList([1, 2, 3], dvals)
     sleep(1)
     timer += 1
 
-
 print ('Done')
-myBox.digitalOutList(dvals[0], [0]*len(dvals[0]))
+myBox.digitalOutList([1, 2, 3], [0, 0, 0])
 myBox.shutDown()
 
 
