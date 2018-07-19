879dc6905c421a5415b238120a4bf935ee2645a3
ekayiigirwa <ekayigirwa@agnesscott.edu>
2018-07-19 16:19:11 -0400

    Update controlBox.py

--- robot-arm-python/controlBox.py
+++ robot-arm-python/controlBox.py
@@ -1,25 +1,30 @@
+''' @brief ControlBox class which sets up the control box on a general level
+    @author Eduige Kayigirwa
+    @date July 2018
+    
+'''
 
 import serial
 import numpy as np
 import scipy as sci
 from scipy.interpolate import interp1d
+from time import sleep
 
-## Documentation for class ControlBox
+# Documentation for class ControlBox
 
 class ControlBox:
-    # class properties
-    alphaLims = np.array([-90,0,90])
-    musecLims = np.array([512,1600,2437])
     
     ## The constructor.
+    ## @param portName is the serial port connected
+    ## @param baud represents the baudrate 
     def __init__ (self, portName, baud):
         self.ser = serial.Serial()
         self.ser.port = portName
         self.ser.baudrate = baud
         
-    # the method of connecting
-    # method Documentation
-    #@param self is the object pointer 
+        
+    ## the method to connect the control box to the computer
+    ## @param self is the object pointer 
     def connect(self):
         self.ser.open()
 
@@ -27,58 +32,101 @@ class ControlBox:
     ## @param mid is the id number connected to the motor
     ## @param mcmd is the rotation direction of the motor     
        
+    def change_int_to_list(self, num):
+        myList = []
+        if isinstance(num,int):
+            myList.append(num)
+            num = myList
+            return num
+    def setServos(self, alphaIds, command, speed = 255):
+        
+        # check if the arguments are integers or lists
+        AlphaInt = []
+        CommandInt = []
+        if isinstance (alphaIds, int):
+           alphaIds = self.change_int_to_list(alphaIds)
 
-    def setServos(self,mid, mcmd, speed = 255):
+        if isinstance (command, int):
+            command = self.change_int_to_list(command)
+            
         if speed >0 and speed < 256:
-            if len(mid) == len(mcmd):
-                for i,m in zip(mid, mcmd):
-                    #print ('"{}"'.format('E;m.'+str(i)+'.'+str(m) + ';'))
-                    self.ser.write('"{}"'.format('E;m.'+str(i)+'.'+str(m) + ';'))
-            # what to do if there is no motor connected to the id?
-            #what happens if there is more ids than connected motors?
+            if len(alphaIds) == len(command):
+                for i,m in zip(alphaIds, command):
+                    print ('{}'.format('E;m.'+str(i)+'.'+str(m) + ';'))
+                    self.ser.write('{}'.format('E;m.'+str(i)+'.'+str(m) + '.'+str(speed)+';'))
+           
         else:
             print "speed must be between 0 and 255."
-
+    ## digitalIn method to allow data input
+    ## returns [pins value], [raw values]
+    ## @param function precise what to do with arguments
+    ## @alphaIds is a list of servos you want to communicate to
     def digitalIn(self, function, alphaIds):
-        outPuts = []
+        pins = []
+        outValues = []
         if function == 'i':
             for i in alphaIds:
                 self.ser.write('{}'.format('a.'+str(function)+'.'+str(i) + ';'))
-                print "from digital input---"
-                outPuts.append (self.ser.readline())
-            return outPuts
-            #print '{}'.format('a.'+str(function)+'.'+str(i) + ';')
+                outPut = self.ser.readline()
+                #pins.append(outPut[10])
+                outValues.append(int(outPut[15]))
+                
+            return (outValues)
+            
         else:
             print "The function " + '{}'.format(function).upper() +  " does not exist."
-    
+
+    ## analogIn method to allow data input
+    ## returns [pins value], [raw values], [converted values]
+    ## @param function precise what to do with arguments
+    ## @alphaIds is a list of servos you want to communicate to
     def analogIn(self, function, alphaIds):
-        outPuts = []
+        pins = []
+        rawVals = []
+        conVals = []
         if function == 'a':
             for i in alphaIds:
-                #self.ser.read(4)
                 self.ser.write('{}'.format('E;a.'+str(function)+'.'+str(i) + ';'))
                 print "from analog Input -----"
-                #print self.ser.readline()
-                outPuts.append(self.ser.readline())
-            return outPuts
+                outPut = self.ser.readline()
+                # get just the pins and their 1s and 0s
+                pins.append(outPut[10])
+                rawVals.append(outPut[15:19])
+                conVals.append(outPut[21:25])
+            return (pins, rawVals, conVals)
                 
-        
-                
-                #print '{}'.format('a.'+str(function)+'.'+str(i) + ';')
         else:
-            print "The function " + '{}'.format(function).upper() +  " does not exist."        
+            print "The function " + '{}'.format(function).upper() +  " does not exist."
+
+    ## digitalOut method to writes output commands to serial port
+    ## @param function precise what to do with arguments
+    ## @param d_output precise one pin output to operate on
+    ## @param is_on is a boolean value
+    def digitalOut(self, d_outPut, is_on):
+        self.ser.write('{}'.format('o.' + str(d_outPut) + '.' + str(is_on) + ';'))
+        reply = self.ser.readline()
+        #print '{}'.format('o.' + str(d_outPut) + '.'+ str(is_on)+  ';')
                 
+
+    ## digitalOutList method to writes output commands to serial port
+    ## @param function precise what to do with arguments
+    ## @param dOutputList is a list of pins output to operate on
+    ## @param is_on is a boolean value
+    def digitalOutList(self, dOutputList, is_on):
+        for dOut, val in zip(dOutputList, is_on):
+            self.ser.write('{}'.format('o.' + str(dOut) + '.' + str(val) + ';'))
+            self.ser.readline()
+   
+    ## disable method disables the robot
+    def disable(self):
+        self.ser.write('{}'.format('D;'))
             
-    # the method to close the port    
+    ## the method to close the port    
     def shutDown(self):
         self.ser.close()
 
-    # a method to set the arm
-    
-    def setArm(self):
-        ticks = interp1d(self.alphaLims, self.musecLims, axis = 0,
-                         fill_value = "extrapolate")
-        print "These are the angles and corresponding positions"
-
-
-
+    @staticmethod
+    def debugger():
+        ser = serial.Serial('COM3', 9600)
+        ser.write('{}'.format(';'))
+        
